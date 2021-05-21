8d7bf979329ebacbb8f1631b499b4d574ea15b8d
Noah Parzentny <78926924+Taikador@users.noreply.github.com>
2021-05-21 19:24:42 +0200

    Now the bot can read the guild id

--- my_bot/config.py
+++ my_bot/config.py
@@ -3,7 +3,6 @@ from types import SimpleNamespace
 from typing import List
 
 
-
 class ConfigError(Exception):
     def __init__(self, *missing_keys: object) -> None:
         self.missing_keys = missing_keys
@@ -16,16 +15,17 @@ class Config(SimpleNamespace):
     def __init__(self):
         config = self._get_config()
         try:
-            self.token: str = config['token']
-            self.api_key: str = config['api_key']
+            self.token: str = config["token"]
+            self.api_key: str = config["api_key"]
 
             self.moderation_roles: List[int] = [
-                int(role_id)
-                for role_id in config['moderation_roles']
+                int(role_id) for role_id in config["moderation_roles"]
             ]
-            self.prefix: str = config['bot_prefix']
+            self.guild_id: int = int(config["guild_id"])
+            self.prefix: str = config["bot_prefix"]
 
-            self.welcome_channel: str = config['welcome_channel']
+            self.rota_channel_id: str = config["rota_channel_id"]
+            self.welcome_channel_id: str = config["welcome_channel_id"]
         except KeyError as error:
             raise ConfigError(*error.args)
 
--- my_bot/main.py
+++ my_bot/main.py
@@ -1,4 +1,4 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 """
 The MIT License (MIT)
@@ -27,6 +27,7 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
+from discord.channel import TextChannel
 import logs
 import asyncio
 import discord
@@ -35,6 +36,7 @@ from pathlib import Path
 from os import listdir
 from logging import getLogger
 from config import Config
+from tinydb import TinyDB
 
 
 from discord import Guild
@@ -42,55 +44,81 @@ from discord.ext import commands
 from discord.ext.commands import Cog
 from discord.ext.commands.errors import CheckFailure, CommandNotFound
 
-log = getLogger('mybot')
+log = getLogger("mybot")
+
 
 class Bot(commands.Bot):
-
     def __init__(self, config: Config, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         logs.setup()
         self.config = config
+        self.db = TinyDB("db.json")
 
-        self.extension_path: Path = Path(__file__).parent / 'extensions'
+        self.extension_path: Path = Path(__file__).parent / "extensions"
 
-        self.guild: Optional[Guild] = None
         self._guild_available = asyncio.Event()
+        self.guild: Optional[Guild] = None
+        self.rota_channel_available = asyncio.Event()
+        self.rota_channel: Optional[TextChannel] = None
+        self.welcome_channel_available = asyncio.Event()
+        self.welcome_channel: Optional[TextChannel] = None
 
-        self.signature = 'Bot created by: Taikador'
+        self.signature = "Bot created by: Taikador"
 
         self.load_extensions()
 
     def load_extensions(self) -> None:
-    
+
         for file in listdir(self.extension_path):
             if file.endswith(".py"):
                 name = file[:-3]
-                self.load_extension(
-                    f'extensions.{name}'
-                )
+                self.load_extension(f"extensions.{name}")
 
     def add_gog(self, cog: Cog) -> None:
         super().add_cog(cog)
-        log.info(f'Cog loaded: {cog.qualified_name}')
+        log.info(f"Cog loaded: {cog.qualified_name}")
 
     async def on_ready(self) -> None:
-        log.info(f'Logged in as {self.user}')
+        log.info(f"Logged in as {self.user}")
 
         await self.wait_until_guild_available()
-        log.info('Now serving in guild: ' + next(guild.name for guild in self.guilds if guild.id == bot.config.guild_id))
-    
+        log.info(
+            "Now serving in guild: "
+            + next(
+                guild.name for guild in self.guilds if guild.id == bot.config.guild_id
+            )
+        )
+
+        self.rota_channel = self.get_channel(config.rota_channel_id)
+        self.rota_channel_available.set()
+        self.welcome_channel = self.get_channel(config.welcome_channel_id)
+        self.welcome_channel_available.set()
+
     async def on_error(self, event: str, *args, **kwargs) -> None:
-        log.exception(f'Unhandled exception in {event}')
+        log.exception(f"Unhandled exception in {event}")
 
     async def on_command_error(self, context, exception) -> None:
         if isinstance(exception, CheckFailure):
-            log.info(f'Check failed for user {context.author}: {exception}')
+            log.info(f"Check failed for user {context.author}: {exception}")
         elif isinstance(exception, CommandNotFound):
             pass
         else:
             log.exception(f'Unhandled "{type(exception)}" exception: {exception}')
-    
+
+    async def on_guild_available(self, guild: Guild) -> None:
+        if guild.id != config.guild_id:
+            return
+
+        self.guild = self.get_guild(config.guild_id)
+        self._guild_available.set()
+
+    async def on_guild_unavailable(self, guild: Guild) -> None:
+        if guild.id != config.guild_id:
+            return
+
+        self._guild_available.clear()
+
     async def wait_until_guild_available(self) -> None:
         """
         Wait until the `config.guild_id` guild is available (and the cache is ready).
@@ -99,13 +127,14 @@ class Bot(commands.Bot):
         """
         await self._guild_available.wait()
 
+
 if __name__ == "__main__":
     config = Config()
     bot = Bot(
         config,
-        command_prefix= config.prefix, 
-        case_insensitive=True, 
+        command_prefix=config.prefix,
+        case_insensitive=True,
         intents=discord.Intents.all(),
-        help_command=None
+        help_command=None,
     )
     bot.run(bot.config.token)
