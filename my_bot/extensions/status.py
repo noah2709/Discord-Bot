83d6a01fb04c33202d201e0a774d9abee0cd6e9d
Noah Parzentny <78926924+Taikador@users.noreply.github.com>
2021-05-21 19:23:33 +0200

    fixed a bug in the status cycle

--- my_bot/extensions/status.py
+++ my_bot/extensions/status.py
@@ -4,8 +4,9 @@ from discord.ext import commands, tasks
 from discord.ext.commands.bot import Bot
 import discord
 
-log = getLogger('extensions.Cycle')
-status = cycle(['"Type .help for help"', 'Developed by Taikador'])
+log = getLogger("extensions.Cycle")
+status = cycle(['"Type .help for help"', "Developed by Taikador"])
+
 
 class CycleCog(commands.Cog, name="Cycle"):
     def __init__(self, bot: Bot):
@@ -15,10 +16,11 @@ class CycleCog(commands.Cog, name="Cycle"):
     @tasks.loop(seconds=10)
     async def change_status(self):
         await self.bot.change_presence(activity=discord.Game(next(status)))
-    
+
     @change_status.before_loop
     async def before_change_status(self):
         await self.bot.wait_until_ready()
-        
+
+
 def setup(bot):
     bot.add_cog(CycleCog(bot))
