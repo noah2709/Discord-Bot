e0dc9a0af5b26f7f895d440cc48ff206a42dda97
Noah Parzentny <78926924+Taikador@users.noreply.github.com>
2021-05-21 19:22:58 +0200

    implemented login into the lolinfo cog

--- my_bot/extensions/lolinfo.py
+++ my_bot/extensions/lolinfo.py
@@ -5,16 +5,37 @@ from requests.models import HTTPError
 from riotwatcher import LolWatcher
 from requests import HTTPError
 from discord.ext.commands import Bot
+from tinydb import where
 
-log = getLogger('extensions.lol')
+log = getLogger("extensions.lolinfo")
+
 
 class LolInfoCog(commands.Cog, name="LeagueInfo"):
     def __init__(self, bot: Bot):
         self.watcher = LolWatcher(bot.config.api_key)
         self.bot = bot
+        self.summoner = self.bot.db.table("summoner_names")
 
-    @commands.command()
+    @commands.command(aliases=["li"], description="Shows information about a Summoner")
     async def lolinfo(self, ctx, *, name):
+        if name == "me":
+            registered = [elem["user"] for elem in self.summoner.all()]
+            if ctx.author.id in registered:
+                results = self.summoner.search(where("user") == ctx.author.id)
+                name = results[0]["summoner"]
+            else:
+                embed = discord.Embed(
+                    title="First you need to register yourself!",
+                    description="Use the command .lollogin <Summoner> to register yourself",
+                    colour=discord.Color.dark_red(),
+                )
+                embed.set_author(
+                    name="LOLINFO",
+                    icon_url="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/LoL_icon.svg/256px-LoL_icon.svg.png",
+                )
+                embed.set_footer(text="League of Legends | All Rights reserved")
+                await ctx.send(embed=embed)
+                return
         try:
             summoner = self.watcher.summoner.by_name("euw1", name)
             stats = self.watcher._league.by_summoner("euw1", summoner["id"])
@@ -27,7 +48,7 @@ class LolInfoCog(commands.Cog, name="LeagueInfo"):
                 embed = discord.Embed(
                     title="Summoner: " + summoner["name"],
                     description="Here you get all Information you'll need",
-                    color=0x109319,
+                    colour=discord.Colour.blurple(),
                 )
                 embed.set_author(
                     name="LOLINFO",
@@ -42,7 +63,6 @@ class LolInfoCog(commands.Cog, name="LeagueInfo"):
                 embed.add_field(name="Account Level", value="Level: " + str(lvl))
                 embed.set_footer(text="League of Legends | All Rights reserved")
                 await ctx.send(embed=embed)
-                pass
             else:
                 tier = stats[0]["tier"]
                 rank = stats[0]["rank"]
@@ -80,7 +100,6 @@ class LolInfoCog(commands.Cog, name="LeagueInfo"):
                 )
                 embed.set_footer(text=self.bot.signature)
                 await ctx.send(embed=embed)
-                pass
         except HTTPError:
             summoner = name
             embed = discord.Embed(
