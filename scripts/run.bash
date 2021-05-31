6e752ccd38465cb5a20e40a975063bcad57d35be
Noah Parzentny <78926924+Taikador@users.noreply.github.com>
2021-05-31 15:24:08 +0200

    Fixed errors on restart

--- scripts/run.bash
+++ scripts/run.bash
@@ -1,10 +1,14 @@
 python my_bot/main.py
 
-until [ $? -eq 104] || [ $? -eq 108 ]; do
-    if [$? -eq 104]; then
+EXIT_CODE=$?
+
+while [ $EXIT_CODE -eq 104 ] || [ $EXIT_CODE -eq 108 ]; do
+    if [ $EXIT_CODE -eq 104 ]; then
         python my_bot/main.py
-    elif [$? -eq 108]; then
+        EXIT_CODE=$?
+    elif [ $EXIT_CODE -eq 108 ]; then
         git pull
         python my_bot/main.py
+        EXIT_CODE=$?
     fi
 done
